34a4c85462c2ded9fc8a77608c63c349f00149c3
p01t3rge1st <77957639+p01t3rge1st@users.noreply.github.com>
2025-11-11 15:22:30 +0100

    Implement RunPreprocessor function

---
+++ src/Preprocessor.cpp
@@ -0,0 +1,25 @@
+#include "../inc/Preprocessor.hh"
+#include <cstdlib>
+#include <sstream>
+#include <array>
+#include <memory>
+#include <stdexcept>
+#include <iostream>
+
+std::string RunPreprocessor(const std::string& inputFile) {
+    std::string cmd = "g++ -E -P " + inputFile + " 2>&1";
+    
+    std::array<char, 128> buffer;
+    std::string result;
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    
+    return result;
+}
