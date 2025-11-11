b8e6471dac3a19240bafcb914d09f37dd0d988ec
p01t3rge1st <77957639+p01t3rge1st@users.noreply.github.com>
2025-11-11 15:20:12 +0100

    Add .hh

---
+++ inc/LibInterface.hh
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <dlfcn.h>
+#include <iostream>
+using CreateCmdFunc = AbstractInterp4Command* (*)();
+using GetCmdNameFunc = const char* (*)();
+class LibInterface {
+
+public:
+    std::string libPath;
+    void* handle {nullptr};
+    CreateCmdFunc CreateCmd {nullptr};
+    GetCmdNameFunc GetCmdName {nullptr};
+
+    LibInterface() = default;
+    LibInterface(const std::string &path): libPath(path) {}
+
+    ~LibInterface() {
+        UnloadPlugin();
+        CreateCmd = nullptr;
+        GetCmdName = nullptr;
+    }
+
+    bool LoadPlugin();
+    void UnloadPlugin();
+};
