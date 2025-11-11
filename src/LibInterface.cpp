fc02d544d4e2ac7f850d9e31ad9466b90a8835a3
p01t3rge1st <77957639+p01t3rge1st@users.noreply.github.com>
2025-11-11 15:16:34 +0100

    Add LibInterface class

---
+++ src/LibInterface.cpp
@@ -0,0 +1,34 @@
+#include "../inc/LibInterface.hh"
+
+bool LibInterface::LoadPlugin() {
+        handle = dlopen(libPath.c_str(), RTLD_LAZY);
+        if (!handle) {
+            std::cerr << "dlopen failed for " << libPath << ": " << dlerror() << "\n";
+            return false;
+        }
+        dlerror();
+        CreateCmd = reinterpret_cast<CreateCmdFunc>(dlsym(handle, "CreateCmd"));
+        const char* dlsym_error = dlerror();
+        if (dlsym_error) {
+            std::cerr << "Cannot load symbol CreateCmd: " << dlsym_error << "\n";
+            dlclose(handle);
+            handle = nullptr;
+            return false;
+        }
+        GetCmdName = reinterpret_cast<GetCmdNameFunc>(dlsym(handle, "GetCmdName"));
+        dlsym_error = dlerror();
+        if (dlsym_error) {
+            std::cerr << "Cannot load symbol GetCmdName: " << dlsym_error << "\n";
+            dlclose(handle);
+            handle = nullptr;
+            return false;
+        }
+        return true;
+    }
+
+void LibInterface::UnloadPlugin() {
+    if (handle) {
+        dlclose(handle);
+        handle = nullptr;
+    }
+}
